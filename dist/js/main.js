a6bc59c5609c815a849325994d2ae0dd90eceb96
Sebastian Selea <Sebi@selea.local>
2020-09-03 13:01:56 -0400

    Another commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,26 @@
-document.addEventListener('keypress', write);
+document.addEventListener('keydown', write);
 
 const container = document.querySelector('.container');
 
 let word = '';
 let spanElement;
 
-function write(e) {}
+function write(e) {
+	const keyPressed = e.key;
+	console.log(keyPressed);
+
+	if (word === '') {
+		word += e.key;
+		let spanElement = document.createElement('span');
+		spanElement.innerHTML = word;
+		container.appendChild(spanElement);
+	}
+
+	if (word === ' ') {
+		word = '';
+	}
+
+	let spanElement = container.lastChild;
+	word += e.key;
+	spanElement.innerHTML = word;
+}
