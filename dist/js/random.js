48458b69257cf14cc4611878746c0b06a4b555c2
Sebastian Selea <Sebi@selea.local>
2020-09-05 09:26:41 -0400

    commit

--- dist/js/random.js
+++ dist/js/random.js
@@ -1,32 +1,6 @@
-function Binding(b) {
-	_this = this;
-	this.element = b.element;
-	this.value = b.object[b.property];
-	this.attribute = b.attribute;
-	this.valueGetter = function () {
-		return _this.value;
-	};
-	this.valueSetter = function (val) {
-		_this.value = val;
-		_this.element[_this.attribute] = val;
-	};
+const f1 = {
+	a: 1,
+};
 
-	Object.defineProperty(b.object, b.property, {
-		get: this.valueGetter,
-		set: this.valueSetter,
-	});
-	b.object[b.property] = this.value;
-
-	this.element[this.attribute] = this.value;
-}
-
-var obj = { a: 123 };
-var myElement = document.getElementById('myDomElement');
-new Binding({
-	object: obj,
-	property: 'a',
-	element: myElement,
-	attribute: 'innerHTML',
-});
-
-obj.a = 456;
+var obj = new f1();
+console.log(obj);
